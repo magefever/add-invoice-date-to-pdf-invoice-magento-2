85317ddc0525c802a89c5c5b40bf35ed3bf24546
sameer <sameer@magefever.com>
2020-02-22 04:44:25 -0700

    inital commit

---
+++ Magefever/Pdf/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * registration componenent
+ *
+ * @package Magefever_Pdf
+ * @author  support@magefver.com
+ * @version 100.0.0
+ * @license OSL v3.0
+ */
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magefever_Pdf',
+    __DIR__
+);
